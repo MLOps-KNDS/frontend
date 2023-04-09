40b7bf6f54120276ad2af5de4e9ba18e01a71d82
radekaadek <radekaadek@gmail.com>
2023-04-09 18:55:09 +0200

    /page

--- app/src/routes/models/+page.svelte
+++ app/src/routes/models/+page.svelte
@@ -1,14 +1,17 @@
-
 <Navbar/>
-<Models/>
+<div class="relative">
+    <Models position="absolute"/>
+</div>
+<Background position="relative"/>
 
 <style>
     :global(body) {
-    background-color: #FEE2E2;
+        background-color: #FEE2E2;
     }
 </style>
 
 <script lang="ts">
     import Navbar from '../../Navbar.svelte';
     import Models from '../../Models.svelte';
+    import Background from '../../Background.svelte';
 </script>
