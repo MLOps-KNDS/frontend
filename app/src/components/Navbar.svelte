94ed58db1ed3b96cb82b8d46c0d06a02dbee51f9
radekaadek <radekaadek@gmail.com>
2023-04-10 10:40:52 +0200

    readability

--- app/src/components/Navbar.svelte
+++ app/src/components/Navbar.svelte
@@ -1,12 +1,21 @@
 <div class="shadow-md bg-gray-200 rounded-md py-1 Navbar">
     <div class="flex mb-3 m-1">
         <!-- on click redirecto to root -->
-    <img src="https://avatars.githubusercontent.com/u/128986668?s=88&v=4" class="h-12 w-12 mr-2 ml-3" alt="Project logo" on:mousedown={() => window.location.href = '/'}>
+    <img src="https://avatars.githubusercontent.com/u/128986668?s=88&v=4"
+        class="h-12 w-12 mr-2 ml-3"
+        alt="Project logo"
+        on:mousedown={() => window.location.href='/'}>
     {#each Tabs as tab}
-        <button class="mx-4 text-gray-500 hover:text-blue-700 focus:text-gray-800 font-medium text-3xl px-2 focus:outline-none" class:active="{activeTab === tab}" on:mousedown={() => window.location.href = '/' + tab.toLowerCase()}>{tab}</button>
+        <button class="mx-4 text-gray-500 hover:text-blue-700 focus:text-gray-800 font-medium text-3xl px-2 focus:outline-none"
+        class:active="{activeTab === tab}"
+        on:mousedown={() => window.location.href = '/' + tab.toLowerCase()}>{tab}</button>
     {/each}
-    <img src="https://avatars.githubusercontent.com/u/11145684?s=200&v=4" class="h-12 w-12 ml-auto" alt="Organization logo" on:mousedown={() => window.location.href = "https://github.com/Kolo-Naukowe-Data-Science-PW"}>
-    <img src="https://avatars.githubusercontent.com/u/77837063?v=4" class="h-12 w-12 mx-3" alt="Profile logo" on:mousedown={() => window.location.href = "/profile"}>
+    <img src="https://avatars.githubusercontent.com/u/11145684?s=200&v=4"
+        class="h-12 w-12 ml-auto" alt="Organization logo"
+        on:mousedown={() => window.location.href="https://github.com/Kolo-Naukowe-Data-Science-PW"}>
+    <img src="https://avatars.githubusercontent.com/u/77837063?v=4"
+        class="h-12 w-12 mx-3" alt="Profile logo"
+        on:mousedown={() => window.location.href = "/profile"}>
     </div>
 </div>
 
--- app/src/routes/datasets/+page.svelte
+++ app/src/routes/datasets/+page.svelte
@@ -1,7 +1,7 @@
 <Navbar/>
 <div class="relative">
     <div class="absolute top-0 left-0 w-full h-full">
-        
+        <!-- The future content goes here -->
     </div>
 </div>
 <Background position="relative"/>
