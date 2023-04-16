653f1aa5d929e1ffd5ec3b19fbebda8403a708de
radekaadek <radekaadek@gmail.com>
2023-04-16 18:32:09 +0200

    file upload errors

--- app/src/components/FileUpload.svelte
+++ app/src/components/FileUpload.svelte
@@ -1,38 +1,57 @@
 <script lang="ts">
-    import { Dropzone, Fileupload } from 'flowbite-svelte'
+    import { Alert, Fileupload } from 'flowbite-svelte'
 
-    let file: FileList | undefined = undefined;
-    let files: File[] = [];
+    let file: FileList | undefined = undefined; // the file that is uploaded
+    let files: File[] = []; // the file that is saved to process
+    let error: string = '';
     const maxSize = 800000;
 
     const checkFile = (file: File) => {
         const validTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/svg+xml'];
         if (validTypes.indexOf(file.type) === -1) {
-            alert('Invalid file type');
+            error = 'Invalid file type';
             return false;
         }
         // check size
         if (file.size > maxSize) {
-            alert('File is too large');
+            error = 'File is too large';
             return false;
         }
         return true;
     }
     
     $: {
-        file = file;
         if (file !== undefined && checkFile(file[0])) {
+            error = '';
             files[0] = file[0];
+        }
+        else {
+            files = [];
         }
     }
 
 </script>
+
+<!-- if an error has been set -->
+{#if error}
+    <Alert color="red">
+        <span class="font-medium">Error! {error}</span> {error}
+    </Alert>
+{/if}
+
+<!-- if there is a file sucessfully saved -->
+{#if files.length > 0}
+    <Alert color="green">
+        <span class="font-medium">Success!</span> The file has been sucessfully submitted.
+    </Alert>
+{/if}
+
 <div class="w-1/4 center">
     <Fileupload bind:files={file} inputClass="border !p-0 dark:text-gray-400"/>
 </div>
+
 {#if files}
     {#each files as f}
-        <p>{f.name}</p>
         <img src={URL.createObjectURL(f)} alt={f.name} />
     {/each}
 {/if}
